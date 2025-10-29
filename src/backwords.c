8114c10176764b382b242e49612a4811a5cb3ff7
Crisjk <c081526@hotmail.com>
2025-10-29 15:56:54 -0500

    Actualizar backwords.c

--- src/backwords.c
+++ src/backwords.c
@@ -2,15 +2,16 @@
 #include <string.h>
 
 int main(void) {
-    char word[10];
-    printf("write a word, the limit are 10 letters");
-    scanf("%s", word);
+    char word[101];
+    printf("write a word, the limit are 100 letters");
+    fflush(stdout);
+    scanf("%s100", word);
     int num = strlen(word);
-    int fix = num - 1;
+    int len = num - 1;
     printf("\n");
     while (fix >= 0) {
-     printf("%c", word[fix]); 
-     fix--;
+     printf("%c", word[len]); 
+     len--;
     }
     return 0;
 }
